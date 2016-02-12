e071f84ddda6d52176c936f71673c947bbe71517
unifisa <unifisaplanilha@gmail.com>
2016-02-12 16:28:12 -0200

    .

--- andersontui/Controllers/HomeController.cs
+++ andersontui/Controllers/HomeController.cs
@@ -17,19 +17,7 @@ namespace Planilhas.Controllers
             return View();
         }
 
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
-
-        //    return View();
-        //}
-
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
-        //}
+      
     }
 
 
