8285f74801b1162b889432d997dff31be273083e
unifisa <unifisaplanilha@gmail.com>
2016-02-17 16:49:12 -0200

    Alterei o nome do banco de dados

--- andersontui/Models/UserAccount.cs
+++ andersontui/Models/UserAccount.cs
@@ -55,9 +55,9 @@ namespace Planilhas.Models
     {
         [Key]
         public int UserRoleId { get; set; }
-        public int UserId { get; set; }
-        public short RoleId { get; set; }
+        public int UserID { get; set; }
+        public int RoleId { get; set; }
 
-        public virtual Role Role { get; set; }
+        
     }
 }
